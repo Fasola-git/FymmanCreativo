c66c8187ad265e1fa116f3d4f453c1117d168676
Marcos Solá <marcosleandrofasola@gmail.com>
2025-04-21 14:23:00 -0300

    fix deploy img

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FymmanCreativo/', // 👈 esta línea es CLAVE para GitHub Pages
   plugins: [react()],
 })
